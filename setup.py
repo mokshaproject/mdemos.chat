0cccc5d3c2fcea6dd5860ab12190c5a96cb5e50b
Ralph Bean <rbean@redhat.com>
2012-05-17 17:44:46 -0400

    Version bump.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except ImportError:
 
 setup(
     name="mdemos.chat",
-    version="0.1.0",
+    version="0.1.1",
     url="http://moksha.fedorahosted.org",
     description="Moksha Chat App",
     long_description="",
